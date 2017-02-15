36cf051109299d53d7a75dafe3c60ee9d03c52f6
sumit rawat <sr674@afsconnect1.njit.edu>
2017-02-15 10:31:54 -0500

    updated

--- display_discount.php
+++ display_discount.php
@@ -1,7 +1,21 @@
 <?php
-$description = $_POST['description'];
-$price = $_POST['price'];
-$discount = $_POST['discount'];
+	// get the data from the form
+        $product_description = $_POST['product_description'];
+	$list_price = $_POST['list_price'];
+	$discount_percent = $_POST['discount_percent'];
+		
+	// calculate the discount
+        $discount = $list_price * $discount_percent * .01;
+	$discount_price = $list_price - $discount;
+	
+	$list_price_formatted = "$".number_format($list_price, 2);
+	$discount_percent_formatted = $discount_percent."%";
+	$discount_formatted = "$".number_format($discount, 2);
+	$discount_price_formatted ="$".number_format($discount_price, 2);
+											
+	 // escape the unformatted input
+	 $product_description_escaped =htmlspecialchars($product_description);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,22 +25,21 @@ $discount = $_POST['discount'];
 </head>
 <body>
     <main>
-        <h1>This page is under construction</h1>
-
+        <h1>Product Discount Calculator</h1>
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $product_description_escaped; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $list_price_formatted; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_percent_formatted; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_formatted; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_price_formatted; ?></span><br>
     </main>
 </body>
 </html>
